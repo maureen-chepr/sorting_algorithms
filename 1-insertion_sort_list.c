43c2fc8f69bf717b4dbbbc4748761b3d3551f7be
maureen-chepr <maureenchep95@gmail.com>
2023-11-20 21:26:09 +0300

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,57 +1,39 @@
 #include "sort.h"
 /**
- * swap - swap 2 element in an list
- * @head: head of list
- * @x: node
- * @y: node
- */
-void swap(listint_t *x, listint_t *y, listint_t **head)
-{
-	listint_t *aux1 = NULL, *aux2 = NULL;
-
-	if (x == NULL || y == NULL)
-		return;
-	aux1 = x->prev;
-	aux2 = y->next;
-	
-	if (aux1)
-		aux1->next = y;
-	if (aux2)
-		aux2->prev = x;
-	x->next = aux2;
-	x->prev = y;
-	y->next = x;
-	y->prev = aux1;
-	if (aux1 == NULL)
-		*head = y;
-}
-/**
- * insertion_sort_list  - insertion_sort_list
- * @list: doubly liked list
+ * insertion_sort_list - insertion sort algorithm
+ * @list: ppointer to head node
  *
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head, *prev;
-	int value;
+	listint_t *head, *temp, *swap, *new;
 
-	if (list == NULL || (*list)->next == NULL || (*list) == NULL)
-	{
-		return;
-	}
 	head = *list;
-	while (head)
+	temp = head->next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	while (temp)
 	{
-		prev = head->prev;
-				value = head->n;
-
-		while (prev && prev->n > value)
+		new = temp->next;
+		while (head && temp->n < head->n)
 		{
-			swap(prev, head, list);
+			if (head->prev)
+				head->prev->next = temp;
+			else
+				*list = temp;
+			if (temp->next)
+				temp->next->prev = head;
+			swap = temp->next;
+			temp->next = head;
+			temp->prev = head->prev;
+			head->next = swap;
+			head->prev = temp;
 			print_list(*list);
-			prev = head->prev;
+			head = temp->prev;
+
 		}
-		head = head->next;
+		temp = new;
+		if (temp)
+			head = temp->prev;
 	}
 }
-
