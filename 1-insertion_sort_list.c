72622d9d60233ea7279729cfc6d2e62b073079e7
maureen-chepr <maureenchep95@gmail.com>
2023-11-20 21:15:08 +0300

    try

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,24 +1,25 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - function that sorts a doubly linked list
- * of integers in ascending order
- * @list: integers to sort
- * Return: void
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       in ascending order using Insertion sort
+ * @list: Pointer to the head of the doubly linked list
  */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *temp;
 
+	/* Check if list is empty or has 1 element */
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
+
 	current = (*list)->next;
 
 	while (current != NULL)
 	{
 		temp = current;
 		prev = current->prev;
+
 		while (prev != NULL && prev->n > temp->n)
 		{
 			/* Swap nodes */
@@ -33,9 +34,11 @@ void insertion_sort_list(listint_t **list)
 				temp->prev->next = temp;
 			else
 				*list = temp;
-			print_list(*list);
+
+			print_list(*list); /* Print the list after each swap */
 			prev = temp->prev;
 		}
+
 		current = current->next;
 	}
 }
