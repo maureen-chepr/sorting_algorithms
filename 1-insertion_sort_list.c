b29f6b026d92d60568c594e859e38e4966f95c76
maureen-chepr <maureenchep95@gmail.com>
2023-11-20 21:12:11 +0300

    up

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,8 +11,8 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *temp;
 
-		if (list == NULL || *list == NULL || (*list)->next == NULL)
-			return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 	current = (*list)->next;
 
 	while (current != NULL)
